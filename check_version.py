f1dc677e559b24a405039809fdc329a22c5e0106
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2024-01-15 22:53:39 +0100

    Added GitHub Actions For publishingn on PyPi Dependabot CodeQL Labeling PR

---
+++ check_version.py
@@ -0,0 +1,24 @@
+import requests
+from spam_detector_ai import __version__, __package_name__, __test_version__
+
+
+def check_package_version(package_name, current_version):
+    version_exists = False
+
+    if __test_version__:
+        print("Test version, skipping PyPI check.")
+        version_exists = True
+    else:
+        response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+
+        if response.status_code == 200:
+            released_versions = response.json()["releases"].keys()
+            if current_version in released_versions:
+                print(f"Version {current_version} already exists on PyPI!")
+                version_exists = True
+
+    # Output for GitHub Actions
+    print(f"::set-output name=version_exists::{version_exists}")
+
+
+check_package_version(__package_name__, __version__)
--- spam_detector_ai/__init__.py
+++ spam_detector_ai/__init__.py
@@ -5,3 +5,12 @@ from . import loading_and_processing
 from . import prediction
 from . import test_and_tuning
 from . import training
+
+__author__ = "Adams Pierre David"
+__author_email__ = "adamspd.developer@gmail.com"
+__author_website__ = "https://adamspierredavid.com/"
+__description__ = "Detecting and filtering spam messages using Machine Learning models."
+__package_name__ = "spam-detector-ai"
+__url__ = "https://github.com/adamspd/spam-detection-project"
+__version__ = "1.0.3"
+__test_version__ = True
