c383e45c65702269283bd2ae72748cf37cbd1415
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2024-01-17 23:49:30 +0100

    Update package version

--- spam_detector_ai/__info__.py
+++ spam_detector_ai/__info__.py
@@ -4,5 +4,5 @@ __author_website__ = "https://adamspierredavid.com/"
 __description__ = "Detecting and filtering spam messages using Machine Learning models."
 __package_name__ = "spam-detector-ai"
 __url__ = "https://github.com/adamspd/spam-detection-project"
-__version__ = "1.0.3"
-__test_version__ = True
+__version__ = "2.0.0"
+__test_version__ = False
