029433c9d917be08efc0bead4ec0efc1b1331278
Adams Pierre David <57180807+adamspd@users.noreply.github.com>
2024-02-04 22:22:17 +0100

    Fixing tag

--- spam_detector_ai/__init__.py
+++ spam_detector_ai/__init__.py
@@ -4,5 +4,5 @@ __author_website__ = "https://adamspierredavid.com/"
 __description__ = "A package for detecting and filtering spam messages using Machine Learning models."
 __package_name__ = "spam-detector-ai"
 __url__ = "https://github.com/adamspd/spam-detection-project"
-__version__ = "2.1.11"
+__version__ = "2.1.12"
 __test_version__ = False
